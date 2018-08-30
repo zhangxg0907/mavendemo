d605f7185b572125c937a05751a7b49d56f9a810
zhangxg0907 <zhangxg0907@163.com>
2018-08-30 14:20:58 +0800

    test1

--- eurekaclientdemo/src/main/java/com/example/demo/DemoApplication.java
+++ eurekaclientdemo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,28 +1,31 @@
 package com.example.demo;
 
+
+import com.example.mapper.UserMapper;
+import com.example.model.User;
 import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.ComponentScan;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.mapper.UserMapper;
-import com.example.model.User;
+
 
 //@SpringBootApplication
 @EnableEurekaClient
 @RestController
-@MapperScan("com.example.mybatisspring.mapper")
+@MapperScan("com.example.mapper")
 //@MapperScan("com.winter.mapper")
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 
-@Service
+//@Service
 public class DemoApplication {
 
 	public static void main(String[] args) {
@@ -32,9 +35,10 @@ public class DemoApplication {
 	@Value("${server.port}")
 	String port;
     //UserMapper.insert();
-	//@Autowired
-	//private UserMapper usermapper;//这里会报错，但是并不会影响
-    //private User user;
+	@Autowired
+
+	private UserMapper usermapper;//这里会报错，但是并不会影响
+    private User user;
 	@RequestMapping("/hi")
 	public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
        //user.setUser("zxg");
